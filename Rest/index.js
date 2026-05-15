f58967ff9bb4b22051b7378bd91bf6f924916ae6
Md. Asheke Rabbi <120050267+ashikin01@users.noreply.github.com>
2026-05-15 11:06:38 +0600

    upload

---
+++ Rest/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+
+const port = 8080;
+const app=express();
+
+app.set("view engine","ejs");
+app.set("views",path.join(import.meta.dirname, "/views"));
+app.use(express.static(path.join(import.meta.dirname,"/public"))); 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.listen(port,()=>{ 
+    console.log("Port is listening");
+})
+
+let posts=[
+    {
+        username:"Asheke",
+        post:"We all love Prophet Mohammad (pbuh)"
+    },
+    {
+        username:"Nafi",
+        post:"We all love cats"
+    },
+    {
+        username:"Shimu",
+        post:"We are human"
+    },
+]
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
